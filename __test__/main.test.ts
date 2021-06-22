6c1b8331d418cca4709c4eff83cf5c1a00694ca6
Alex Teodorescu <ateodorescu721@gmail.com>
2021-06-22 12:32:44 -0400

    add tests to signup endpoint

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -3,3 +3,77 @@ import request, { Test } from 'supertest';
 import { Server } from 'http';
 import bootServer from '../server';
 import bootDB from '../db';
+import { seedDB } from '../__seed__';
+import { UserEntry } from '../models/user';
+
+const port = Number(process.env.TEST_PORT);
+const connectionString = String(process.env.TEST_DB_CONN);
+
+let server: Server;
+let db: Mongoose | undefined;
+let mockUsers: UserEntry[];
+
+interface MockNewUser {
+  email: string,
+  password: string,
+  username: string
+}
+
+beforeAll(async () => {
+  db = await bootDB(connectionString);
+  if (db) {
+    await db.connection.db.dropDatabase();
+    mockUsers = await seedDB(db);
+  }
+  server = bootServer(port);
+});
+
+test('Mock users must be present', () => {
+  expect(mockUsers).toHaveLength(25);
+  expect(mockUsers[0].recipeStore).toHaveLength(4);
+  expect(mockUsers[0].recipeStore[0].notes).toHaveLength(2);
+});
+
+describe('POST /signup', () => {
+  let endpoint: Test;
+  const newUser: MockNewUser = {
+    email: 'charley@chefshare.com',
+    password: 'password123',
+    username: 'charcarr'
+  };
+
+  beforeEach(() => {
+    endpoint = request(server).post('/signup');
+  });
+
+  test('Returns 400 for missing data', async () => {
+    const response = await endpoint.send({});
+    expect(response.status).toBe(400);
+  });
+
+  test('Returns a token for a new user', async () => {
+    const response = await endpoint.send(newUser);
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('accessToken');
+  });
+
+  test('Returns 403 if email already exists', async () => {
+    const response = await endpoint.send(newUser);
+    expect(response.status).toBe(403);
+  });
+
+  test('Return 409 if username already exists', async () => {
+    const sameUsername: MockNewUser = {
+      email: 'char@chefshare.com',
+      password: 'password123',
+      username: 'charcarr'
+    };
+    const response = await endpoint.send(sameUsername);
+    expect(response.status).toBe(409);
+  })
+});
+
+afterAll(async () => {
+  await db?.connection.close();
+  server.close();
+});
